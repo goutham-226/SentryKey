eb37ce80c17edbc80a4ca448ca874ba909486235
goutham-226 <67628459+goutham-226@users.noreply.github.com>
2026-09-08 22:37:13 +0530

    chore: cleaning sql dir

--- sql/001_schema.sql
+++ sql/001_schema.sql
@@ -5,3 +5,23 @@ CREATE TABLE users(
     created_on TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
+CREATE TABLE api_keys(
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    key_prefix TEXT NOT NULL,
+    key_hash TEXT NOT NULL UNIQUE,
+    monthly_quota BIGINT DEFAULT 100000,
+    revoked_on TIMESTAMPTZ,
+    created_on TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE usage_records(
+    id BIGSERIAL PRIMARY KEY,
+    api_key_id BIGINT NOT NULL REFERENCES api_keys(id) ON DELETE CASCADE,
+    requested_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    token_count BIGINT NOT NULL,
+    status_code SMALLINT NOT NULL,
+    latency_ms INTEGER  NOT NULL
+);
+ 
+
